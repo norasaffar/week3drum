a2b338f1ae8a8f448b220f053b423ba65e866d0e
NoraSaffar <norasaffar8910@gmail.com>
2023-11-14 16:17:20 +0000

    MESSAGEY

---
+++ script.js
@@ -0,0 +1,59 @@
+const boom = document.getElementById("boom");
+
+boom.addEventListener('click', () => {
+    new Audio("./sounds/boom.wav").play();
+})
+
+
+const clap = document.getElementById("clap");
+
+clap.addEventListener('click', () => {
+    new Audio("./sounds/clap.wav").play();
+})
+
+
+const hihat = document.getElementById("hihat");
+
+hihat.addEventListener('click', () => {
+    new Audio("./sounds/hihat.wav").play();
+})
+
+
+const kick = document.getElementById("kick");
+
+kick.addEventListener('click', () => {
+    new Audio("./sounds/kick.wav").play();
+})
+
+
+const openhat = document.getElementById("openhat");
+
+openhat.addEventListener('click', () => {
+    new Audio("./sounds/openhat.wav").play();
+})
+
+
+const ride = document.getElementById("ride");
+
+ride.addEventListener('click', () => {
+    new Audio("./sounds/ride.wav").play();
+})
+
+const snare = document.getElementById("snare");
+
+snare.addEventListener('click', () => {
+    new Audio("./sounds/snare.wav").play();
+})
+
+const tink = document.getElementById("tink");
+
+tink.addEventListener('click', () => {
+    new Audio("./sounds/tink.wav").play();
+})
+
+
+const tom = document.getElementById("tom");
+
+tom.addEventListener('click', () => {
+    new Audio("./sounds/tom.wav").play();
+})
